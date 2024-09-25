3b1c3a2c5f07b49fb1cd18b9af3f01157ea0e918
silvia-ramos10 <silviasrmc@gmail.com>
2024-09-25 11:53:59 -0300

    hbj

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ function areaQuadrado(lado){
     return areaQuadrado(lado**2)
 }
 console.log(areaQuadrado(9));
+let media=funcion soma(num1,num2){
+    return num1+num2;
+}
+console.log(soma(34,67))
