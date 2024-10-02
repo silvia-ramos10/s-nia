fb37890ff4fc5945cf98981abbb28bb33e282723
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-02 11:55:49 -0300

    gju,

--- main.js
+++ main.js
@@ -13,4 +13,9 @@
 // }
 // console.log(media(2,4,3));
 // parseInt(prompt('?'))
-let estados=['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+let i = 0;
+while (i < 5) {
+    console.log(i);
+i=i+1;
+}
