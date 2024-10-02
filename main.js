081572baa96c22de6764de6da1a5acff29f07205
silvia-ramos10 <silviasrmc@gmail.com>
2024-10-02 11:32:09 -0300

    khb

--- main.js
+++ main.js
@@ -13,4 +13,4 @@
 // }
 // console.log(media(2,4,3));
 // parseInt(prompt('?'))
-let estados['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+let estados=['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
